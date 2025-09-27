bbbdbfb33933fd9e185d41153808cce7ecae28e5
ChanVimean <mrrmeanvlogger@gmail.com>
2025-09-27 10:25:36 +0700

    add shadcn ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
+
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+
+      <Button variant="outline">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="primary">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="none">Click me</Button>
+      <button className="text-blue-600">Custom</button>
+
+      <Checkbox></Checkbox>
+
+    </div>
   )
 }
 
