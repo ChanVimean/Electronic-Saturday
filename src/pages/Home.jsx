c16267d94530f1566c871164bcc3a8768052fd00
ChanVimean <mrrmeanvlogger@gmail.com>
2025-09-27 10:58:20 +0700

    add footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,10 @@
-
-import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
 
-      <Button variant="outline">Click me</Button>
-      <Button variant="ghost">Click me</Button>
-      <Button variant="primary">Click me</Button>
-      <Button variant="secondary">Click me</Button>
-      <Button variant="none">Click me</Button>
-      <button className="text-blue-600">Custom</button>
 
-      <Checkbox></Checkbox>
 
     </div>
   )
