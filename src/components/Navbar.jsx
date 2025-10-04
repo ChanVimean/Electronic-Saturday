b7e3f210c3eb323675d615984ca168c1d8898d5a
ChanVimean <mrrmeanvlogger@gmail.com>
2025-10-04 09:53:24 +0700

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,28 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex">
-      <section>
-        <img src="logo.png" alt="logo" className="h-16" />
-      </section>
+    <nav className="bg-blue-200">
+      <div className="max-w-7xl mx-auto flex justify-between items-center py-2 px-6">
+        <section>
+          <img src="logo.png" alt="logo" className="h-16" />
+        </section>
 
-      <section>
-        <Link to="/">Home</Link>
-        <Link to="/product">Product</Link>
-        <Link to="/contact">Contact</Link>
-      </section>
+        <section className="hidden lg:block space-x-6 font-semibold text-xl">
+          <Link to="/">Home</Link>
+          <Link to="/product">Product</Link>
+          <Link to="/contact">Contact</Link>
+        </section>
 
-      <section>
-        <img src="profile.webp" alt="profile" className="h-16 rounded-full overflow-hidden" />
-      </section>
+        <section className="flex items-center gap-2 font-medium text-lg">
+          <h2>Tony Stark</h2>
+          <img
+            src="profile.webp"
+            alt="profile"
+            className="h-12 rounded-full overflow-hidden"
+          />
+          <button>theme</button>
+        </section>
+      </div>
     </nav>
   );
 };
